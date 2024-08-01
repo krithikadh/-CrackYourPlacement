42117f6e6f79eb33ecb7477948940bb0228f1b1f
Krithika D H <124453827+krithikadh@users.noreply.github.com>
2024-08-01 19:57:05 +0530

    Create Solution.java

---
+++ Move Zeroes/Solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] moveZeroes(int[] nums) {
+        for(int i=0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[i]==0){
+                    nums[i]=nums[i]^nums[j];
+                    nums[j]=nums[i]^nums[j];
+                    nums[i]=nums[i]^nums[j];
+                }
+            }
+        }
+        return nums;
+    }
+}
