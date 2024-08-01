3c6716146f4b82e83fe5f2bc62884d53b569d8bf
Krithika D H <124453827+krithikadh@users.noreply.github.com>
2024-08-01 19:47:47 +0530

    Create Solution.java

---
+++ Remove Duplicates from Sorted Array/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int n=nums.length;
+        if(n==0){
+            return 0;
+        }
+        int j=1,k=1;
+        for(int i=1;i<n;i++){
+            if(nums[i]!=nums[i-1]){
+                nums[j]=nums[i];
+                j++;
+                k++;
+            }
+        }
+        return k;
+    }
+}
