0cba71f76bd1c5240fc154e6f94f0544ed57e238
Krithika D H <124453827+krithikadh@users.noreply.github.com>
2024-08-07 19:35:55 +0530

    Create Solution.java

---
+++ String/Simplify Path/Solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String simplifyPath(String path) {
+        Stack<String> stack = new Stack<>();
+        String[] components = path.split("/");
+        for (String component : components) {
+            if (component.equals("..")) {
+                if (!stack.isEmpty()) {
+                    stack.pop();
+                }
+            } else if (!component.equals("") && !component.equals(".")) {
+                stack.push(component);
+            }
+        }
+        StringBuilder result = new StringBuilder();
+        for (String dir : stack) {
+            result.append("/").append(dir);
+        }
+        return result.length() > 0 ? result.toString() : "/";
+    }
+}
