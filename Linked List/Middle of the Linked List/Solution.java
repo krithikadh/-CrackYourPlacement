b24a67ab9d231bd2f306e2a752452d8e8e35a331
Krithika D H <124453827+krithikadh@users.noreply.github.com>
2024-08-27 18:32:53 +0530

    Create Solution.java

---
+++ Linked List/Middle of the Linked List/Solution.java
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode s,f,p;
+        s=head;
+        f=head;
+        while(f!=null && f.next!=null){
+            s=s.next;
+            f=f.next.next;
+        }
+        p=s;
+        return p;
+    }
+}
