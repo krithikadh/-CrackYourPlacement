a7f8b661fd3f296c38304747e7558ded67e3bd46
Krithika D H <124453827+krithikadh@users.noreply.github.com>
2024-09-03 20:06:30 +0530

    Create Solution.java

---
+++ Linked List/Reorder List/Solution.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public void reorderList(ListNode head) {
+        if (head == null) {
+            return;
+        }
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode prev = null, curr = slow;
+        while (curr != null) {
+            ListNode temp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
+        }
+        ListNode first = head, second = prev;
+        while (second.next != null) {
+            ListNode temp1 = first.next, temp2 = second.next;
+            first.next = second;
+            first = temp1;
+            second.next = first;
+            second = temp2;
+        }
+    }
+}
